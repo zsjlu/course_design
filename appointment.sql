2e334c51dea9c40d709e9857ecdb5eeda2115273
hellolujian <30590145+hellolujian@users.noreply.github.com>
2017-12-25 11:56:20 +0800

    Update appointment.sql

--- appointment.sql
+++ appointment.sql
@@ -1,52 +1,122 @@
-create table product_info(
-    product_id varchar(32) not null,
-    product_name varchar(64) not null comment '商品名称',
-    product_price decimal(8,2) not null comment '单价',
-    product_stock int not null comment '库存',
-    product_description varchar(64) comment '描述',
-    product_icon varchar(512) comment '小图',
-    product_status tinyint(0) default '0' comment '商品状态，0正常1下架',
-    category_type int not null comment '类目编号',
-    create_time timestamp not null default current_timestamp comment '创建时间',
-    update_time timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (product_id)
-) comment '商品表' ;
 
-create table product_category(
-    category_id int not null auto_increment,
-    category_name varchar(64) not null comment '类目名字',
-    category_type int not null comment '类目编号',
-    create_time timestamp not null default current_timestamp comment '创建时间',
-    update_time timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (category_id),
-    unique key `uqe_category_type` (`category_type`)
-) comment '类目表';
 
-create table order_master(
-    order_id varchar(32) not null,
-    buyer_name varchar(32) not null comment '买家名字',
-    buyer_phone varchar(32) not null comment '买家电话',
-    buyer_adress varchar(128) not null comment '买家地址',
-    buyer_openid varchar(64) not null comment '买家微信openid',
-    order_amount decimal(8,2) not null comment '订单总金额',
-    order_status tinyint(3) not null default '0' comment '订单状态，默认0新下单',
-    pay_status tinyint(3) not null default '0' comment '支付状态，默认0未支付',
-    create_time timestamp not null default current_timestamp comment '创建时间',
-    update_time timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (order_id),
-    key idx_buyer_openid (buyer_openid)
-) comment '订单表';
 
-create table order_detail(
-    detail_id varchar(32) not null,
-    order_id varchar(32) not null,
-    product_id varchar(32) not null,
-    product_name varchar(64) not null comment '商品名称',
-    product_price decimal(8,2) not null comment '商品价格',
-    product_quantity int not null comment '商品数量',
-    product_icon varchar(512) comment '商品小图',
-    create_time timestamp not null default current_timestamp comment '创建时间',
-    update_time timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (detail_id),
-    key idx_order_id (order_id)
-) comment '订单详情表';
+
+create
+table user_info (
+
+    user_id varchar(32) not null comment '读者编号',
+
+    order_status tinyint(2) default '0' comment
+'预约状态',
+
+    sign_in_status tinyint(2) default '0'
+comment '签到状态',
+
+    sign_out_status tinyint(2) default '0'
+comment '签离状态',
+
+    primary key(user_id)
+
+)
+comment '读者信息表' ;
+
+ 
+
+create
+table administrator (
+
+    admin_id varchar(8) not null comment '管理员编号',
+
+    admin_name varchar(64) not null comment '管理员姓名',
+
+    admin_account varchar(64) not null comment
+'管理员账号',
+
+    admin_account varchar(64) not null comment
+'管理员密码',
+
+    primary key(admin_id)
+
+)
+comment '管理员表' ;
+
+ 
+
+create
+table floor (
+
+    floor_id varchar(32) not null comment '楼层编号',
+
+    floor_name varchar(64) not null comment '楼层名称',
+
+    primary key(floor_id)
+
+)
+comment '楼层表' ;
+
+ 
+
+create
+table reading_room (
+
+    reading_room_id varchar(32) not null
+comment '阅览室编号',
+
+    reading_room_id varchar(64) not null
+comment '阅览室名称',
+
+    floor_id varchar(32) comment '阅览室所属楼层',
+
+    seat_count int not null comment '座位数量',
+
+    primary key(reading_room_id),
+
+    foreign key(floor_id) references
+floor(floor_id) on delete set null
+
+)
+comment '阅览室表' ;
+
+ 
+
+create
+table seat (
+
+    seat_id varchar(32) not null comment '座位编号',
+
+    seat_status tinyint(2) default '0' comment
+'使用状态',
+
+    reading_room_id varchar(32) comment '座位所属阅览室',
+
+    primary key(seat_id),
+
+    foreign key(reading_room_id) references
+reading_room(reading_room_id) on delete set null
+
+)
+comment '座位表' ;
+
+ 
+
+create
+table order (
+
+    id int not null auto_increment comment '自增主键',
+
+    user_id varchar(32) not null comment '读者编号',
+
+    seat_id varchar(32) not null comment '座位编号',
+
+    order_time timestamp(8) not null default
+current_timestamp comment '预约时间',
+
+    create_time timestamp not null default
+current_timestamp comment '创建时间',
+
+    primary key(id)
+
+)
+comment '预约表' ;
+
