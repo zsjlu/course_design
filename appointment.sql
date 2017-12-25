48d4931973685043b96421a1b68ec11c2c093f5f
hellolujian <30590145+hellolujian@users.noreply.github.com>
2017-12-25 14:13:56 +0800

    Update appointment.sql

--- appointment.sql
+++ appointment.sql
@@ -1,13 +1,15 @@
+use library_reservation;
+
 create table user_info (
     user_id varchar(32) not null comment '读者编号',
-    order_status tinyint(2) default '0' comment '预约状态',
+    reserve_status tinyint(2) default '0' comment '预约状态',
     sign_in_status tinyint(2) default '0' comment '签到状态',
     sign_out_status tinyint(2) default '0' comment '签离状态',
     primary key(user_id)
 ) comment '读者信息表' ;
  
 create table administrator (
-    admin_id varchar(8) not null comment '管理员编号',
+    admin_id varchar(32) not null comment '管理员编号',
     admin_name varchar(64) not null comment '管理员姓名',
     admin_account varchar(64) not null comment '管理员账号',
     admin_account varchar(64) not null comment '管理员密码',
@@ -37,11 +39,11 @@ create table seat (
     foreign key(reading_room_id) references reading_room(reading_room_id) on delete set null
 ) comment '座位表' ;
 
-create table orders (
+create table reservation (
     id int not null auto_increment comment '自增主键',
     user_id varchar(32) not null comment '读者编号',
     seat_id varchar(32) not null comment '座位编号',
-    order_time timestamp(8) not null default current_timestamp comment '预约时间',
+    reserve_time timestamp(8) not null default current_timestamp comment '预约时间',
     create_time timestamp not null default current_timestamp comment '创建时间',
     primary key(id)
 ) comment '预约表' ;
@@ -50,7 +52,7 @@ create table sign_in (
     id int not null auto_increment comment '自增主键',
     user_id varchar(32) not null comment '读者编号',
     seat_id varchar(32) not null comment '座位编号',
-    order_time timestamp(8) comment '预约时间',
+    reserve_time timestamp(8) comment '预约时间',
     sign_in_time timestamp not null default current_timestamp comment '签到时间',
     sign_in_type tinyint(2) comment '签到类别',
     primary key(id)
@@ -60,7 +62,7 @@ create table sign_out (
     id int not null auto_increment comment '自增主键',
     user_id varchar(32) not null comment '读者编号',
     seat_id varchar(32) not null comment '座位编号',
-    order_time timestamp(8) comment '预约时间',
+    reserve_time timestamp(8) comment '预约时间',
     sign_out_type tinyint(2) comment '签离类别',
     sign_out_time timestamp not null default current_timestamp comment '签离时间',
     primary key(id)
@@ -76,7 +78,7 @@ create table inobservance (
     id int not null auto_increment comment '自增主键',
     user_id varchar(32) not null comment '读者编号',
     seat_id varchar(32) not null comment '座位编号',
-    order_time timestamp(8) comment '预约时间',
+    reserve_time timestamp(8) comment '预约时间',
     inobservance_type int not null comment '违规类别',
     create_time timestamp not null comment default current_timestamp comment '生成记录时间',
     primary key(id)
