7c9743ac312b7453ca2998268c15e8e99bc16514
hellolujian <30590145+hellolujian@users.noreply.github.com>
2017-12-25 11:59:59 +0800

    Update appointment.sql

--- appointment.sql
+++ appointment.sql
@@ -1,84 +1,33 @@
-
-
-
-
-create
-table user_info (
-
+create table user_info (
     user_id varchar(32) not null comment '读者编号',
-
-    order_status tinyint(2) default '0' comment
-'预约状态',
-
-    sign_in_status tinyint(2) default '0'
-comment '签到状态',
-
-    sign_out_status tinyint(2) default '0'
-comment '签离状态',
-
+    order_status tinyint(2) default '0' comment '预约状态',
+    sign_in_status tinyint(2) default '0' comment '签到状态',
+    sign_out_status tinyint(2) default '0' comment '签离状态',
     primary key(user_id)
-
-)
-comment '读者信息表' ;
-
+) comment '读者信息表' ;
  
-
-create
-table administrator (
-
+create table administrator (
     admin_id varchar(8) not null comment '管理员编号',
-
     admin_name varchar(64) not null comment '管理员姓名',
-
-    admin_account varchar(64) not null comment
-'管理员账号',
-
-    admin_account varchar(64) not null comment
-'管理员密码',
-
+    admin_account varchar(64) not null comment '管理员账号',
+    admin_account varchar(64) not null comment '管理员密码',
     primary key(admin_id)
+) comment '管理员表' ; 
 
-)
-comment '管理员表' ;
-
- 
-
-create
-table floor (
-
-    floor_id varchar(32) not null comment '楼层编号',
-
+create table floor (
+    floor_id varchar(32) not null comment '楼层编号',
     floor_name varchar(64) not null comment '楼层名称',
-
     primary key(floor_id)
+) comment '楼层表' ; 
 
-)
-comment '楼层表' ;
-
- 
-
-create
-table reading_room (
-
-    reading_room_id varchar(32) not null
-comment '阅览室编号',
-
-    reading_room_id varchar(64) not null
-comment '阅览室名称',
-
+create table reading_room (
+    reading_room_id varchar(32) not null comment '阅览室编号',
+    reading_room_id varchar(64) not null comment '阅览室名称',
     floor_id varchar(32) comment '阅览室所属楼层',
-
     seat_count int not null comment '座位数量',
-
     primary key(reading_room_id),
-
-    foreign key(floor_id) references
-floor(floor_id) on delete set null
-
-)
-comment '阅览室表' ;
-
- 
+    foreign key(floor_id) references floor(floor_id) on delete set null
+) comment '阅览室表' ;
 
 create
 table seat (
