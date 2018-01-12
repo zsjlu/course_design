38c5cb12bc20e890f74ffbe0f1d283c2538544d2
hellolujian <1370256381@qq.com>
2018-01-12 13:49:37 +0800

    vue

--- reservation.sql
+++ reservation.sql
@@ -1,5 +1,78 @@
 use library_reservation;
 
+create table college (
+    college_id int not null comment '学院编号',
+    college_name varchar(32) not null comment '学院名称',
+    primary key(college_id)
+) comment '学院表';
+
+insert into college(college_id,college_name) values(1001,'计算机科学与通信工程学院');
+insert into college(college_id,college_name) values(1002,'机械工程学院');
+insert into college(college_id,college_name) values(1003,'能源与动力工程学院');
+insert into college(college_id,college_name) values(1004,'材料科学与工程学院');
+insert into college(college_id,college_name) values(1005,'电气信息工程学院');
+insert into college(college_id,college_name) values(1006,'食品与生物工程学院');
+insert into college(college_id,college_name) values(1007,'环境与安全工程学院');
+insert into college(college_id,college_name) values(1008,'理学院');
+insert into college(college_id,college_name) values(1009,'土木工程与力学学院');
+insert into college(college_id,college_name) values(1010,'化学化工学院');
+insert into college(college_id,college_name) values(1011,'管理学院');
+insert into college(college_id,college_name) values(1012,'财经学院');
+insert into college(college_id,college_name) values(1013,'马克思主义学院');
+insert into college(college_id,college_name) values(1014,'文法学院');
+insert into college(college_id,college_name) values(1015,'外国语学院');
+insert into college(college_id,college_name) values(1016,'艺术学院');
+insert into college(college_id,college_name) values(1017,'教师教育学院');
+insert into college(college_id,college_name) values(1018,'临床医学院');
+insert into college(college_id,college_name) values(1019,'基础医学与医学技术学院');
+insert into college(college_id,college_name) values(1020,'药学院');
+insert into college(college_id,college_name) values(1021,'海外教育学院');
+insert into college(college_id,college_name) values(1022,'京江学院');
+insert into college(college_id,college_name) values(1023,'继续教育学院');
+insert into college(college_id,college_name) values(1024,'体育部');
+
+create table degree (
+    degree_id int not null comment '学位编号',
+    degree_name varchar(32) not null comment '学位名称',
+    primary key(degree_id)
+) comment '学位表' ;
+
+insert into degree(degree_id,degree_name) values(11,'本科生');
+insert into degree(degree_id,degree_name) values(12,'研究生');
+insert into degree(degree_id,degree_name) values(13,'博士生');
+insert into degree(degree_id,degree_name) values(14,'教师');
+
+
+create table user (
+    user_id varchar(32) not null comment '读者编号',
+    user_name varchar(32) not null comment '读者姓名',
+    user_password varchar(32) not null comment '登录密码',
+    college_id int default '1001' comment '学院编号，默认计算机学院',
+    degree_id int default '11' comment '身份编号，默认本科生',
+    phone varchar(32) comment '号码',
+    email varchar(32) comment '邮箱',
+    primary key(user_id),
+    foreign key(college_id) references college(college_id) on delete set null,
+    foreign key(degree_id) references degree(degree_id) on delete set null
+) comment '用户表';
+
+insert into user(user_id,user_name,user_password,college_id,degree_id,phone,email) values('3140602023','陆健','123456',1001,11,'18852852189','1370256381@qq.com');
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602024','张家辉','123456',1001,11);
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602025','朱俊豪','123456',1001,11);
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602026','王润','123456',1001,11);
+
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602027','张三','123456',1002,11);    
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602028','李四','123456',1003,11);    
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602029','王五','123456',1004,11);    
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602030','陈六','123456',1005,11);    
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602031','阿七','123456',1006,11);    
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3140602032','陆八','123456',1007,11);   
+
+
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3130602032','老九','123456',1008,12);    
+insert into user(user_id,user_name,user_password,college_id,degree_id) values('3130602033','小十','123456',1009,13);
+
+
 create table user_info (
     user_id varchar(32) not null comment '读者编号',
     reserve_status tinyint(2) default '0' comment '预约状态',
@@ -42,6 +115,10 @@ insert into reading_room(reading_room_id, reading_room_name, floor_id, seat_coun
 insert into reading_room(reading_room_id, reading_room_name, floor_id, seat_count) values('101-1002','自然科学图书阅览室','101',100);
 insert into reading_room(reading_room_id, reading_room_name, floor_id, seat_count) values('101-1003','综合阅览室','101',100);
 
+insert into reading_room(reading_room_id, reading_room_name, floor_id, seat_count) values('101-1004','二楼综合阅览室','102',100);
+insert into reading_room(reading_room_id, reading_room_name, floor_id, seat_count) values('101-1005','二楼密集书库','102',100);
+
+
 create table seat (
     seat_id varchar(32) not null comment '座位编号',
     seat_status tinyint(2) default '0' comment '使用状态',
@@ -52,12 +129,12 @@ create table seat (
 
 insert into seat(seat_id, reading_room_id) values('101-1001-10001','101-1001');
 insert into seat(seat_id, reading_room_id) values('101-1001-10002','101-1001');
-insert into seat(seat_id, reading_room_id) values('101-1001-10003','101-1001');
-insert into seat(seat_id, reading_room_id) values('101-1001-10004','101-1001');
-insert into seat(seat_id, reading_room_id) values('101-1001-10005','101-1001');
-insert into seat(seat_id, reading_room_id) values('101-1001-10006','101-1001');
-insert into seat(seat_id, reading_room_id) values('101-1001-10008','101-1001');
-insert into seat(seat_id, reading_room_id) values('101-1001-10009','101-1001');
+insert into seat(seat_id, reading_room_id) values('101-1001-10003','101-1002');
+insert into seat(seat_id, reading_room_id) values('101-1001-10004','101-1002');
+insert into seat(seat_id, reading_room_id) values('101-1001-10005','101-1004');
+insert into seat(seat_id, reading_room_id) values('101-1001-10006','101-1004');
+insert into seat(seat_id, reading_room_id) values('101-1001-10007','101-1005');
+insert into seat(seat_id, reading_room_id) values('101-1001-10008','101-1005');
 
 create table reservation (
     id int not null auto_increment comment '自增主键',
@@ -77,8 +154,20 @@ create table sign_in (
     reserve_time timestamp comment '预约时间',
     sign_in_time timestamp not null default current_timestamp comment '签到时间',
     sign_in_type tinyint(2) comment '签到类别',
-    primary key(id)
+    primary key(id),
+    foreign key(sign_in_type) references sign_in_type(sign_in_type_id)
 ) comment '签到表' ;
+
+create table sign_in_type (
+    sign_in_type_id tinyint(2) comment '签到类别',
+    sign_in_type_name varchar(32) comment '类别名称',
+    primary key(sign_in_type_id)
+) comment '签到类别表' ;
+
+insert into sign_in_type values(0,"未签到") ;
+insert into sign_in_type values(1,"首次签到") ;
+insert into sign_in_type values(2,"当天临时签离后签到") ;
+
 
 insert into sign_in(user_id, seat_id, reserve_time, sign_in_time, sign_in_type) values('3140602023','101-1001-10001','2017-12-26 10:11:09','2017-12-26 10:12:12',1);
 
@@ -92,18 +181,28 @@ create table sign_out (
     primary key(id)
 ) comment '签离表' ;
 
+create table sign_out_type (
+    sign_out_type_id tinyint(2) comment '签离类别',
+    sign_out_type_name varchar(32) comment '类别名称',
+    primary key(sign_out_type_id)
+) comment '签离类别' ;
+
+insert into sign_out_type values(0,'未签离');
+insert into sign_out_type values(1,'临时签离');
+insert into sign_out_type values(2,'终了签离');
+
 insert into sign_out(user_id, seat_id, reserve_time, sign_out_type, sign_out_time) values('3140602023', '101-1001-10001','2017-12-26 10:11:09',1,'2017-12-26 11:11:11');
 
 create table inobservance_type (
     inobservance_type_id int not null comment '违规类别编号',
-    inboservance_type_name varchar(32) not null comment '违规类别名称',
+    inobservance_type_name varchar(32) not null comment '违规类别名称',
     primary key(inobservance_type_id)
 ) comment '违规类别表' ;
 
-insert into inobservance_type(inobservance_type_id, inboservance_type_name) values(1,'未签到');
-insert into inobservance_type(inobservance_type_id, inboservance_type_name) values(2,'未签离');
-insert into inobservance_type(inobservance_type_id, inboservance_type_name) values(3,'临时离开未归');
-insert into inobservance_type(inobservance_type_id, inboservance_type_name) values(4,'临时离开未签离');
+insert into inobservance_type(inobservance_type_id, inobservance_type_name) values(1,'未签到');
+insert into inobservance_type(inobservance_type_id, inobservance_type_name) values(2,'未签离');
+insert into inobservance_type(inobservance_type_id, inobservance_type_name) values(3,'临时离开未归');
+insert into inobservance_type(inobservance_type_id, inobservance_type_name) values(4,'临时离开未签离');
 
 
 
